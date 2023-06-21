e4974eac886effd11d86d8ab1cde36d89077ff5b
Haris Painesis <95146846+zpainesi@users.noreply.github.com>
2023-06-21 18:54:34 +0300

    Update ZeroBias_Timing.cc

--- TagAndProbe/plugins/ZeroBias_Timing.cc
+++ TagAndProbe/plugins/ZeroBias_Timing.cc
@@ -176,9 +176,9 @@ private:
   TH1F* _tauBxMatched = new TH1F("tauBxMatched", "tauBxMatched", 5, -2.5, 2.5);
   TH1F* _jetBxMatched = new TH1F("jetBxMatched", "jetBxMatched", 5, -2.5, 2.5);
   //TH2F* _muBxMatched_eta_phi = new TH2F("muBxMatched_eta_phi","muBxMatched_eta_phi", 100, -5 , 5, 72,-3.1416,3.1416);
-  TH2F* _egBx0Matched_eta_phi = new TH2F("egBx0Matched_eta_phi","egBx0Matched_eta_phi", 100, -5 , 5, 72,-3.1416,3.1416);
-  TH2F* _egBxMin1Matched_eta_phi = new TH2F("egBxMin1Matched_eta_phi","egBxMin1Matched_eta_phi", 100, -5 , 5, 72,-3.1416,3.1416);
-  TH2F* _egBxPlus1Matched_eta_phi = new TH2F("egBxPlus1Matched_eta_phi","egBxPlus1Matched_eta_phi", 100, -5 , 5, 72,-3.1416,3.1416);
+  TH1F* _egBx0Matched_eta = new TH2F("egBx0Matched_eta","egBx0Matched_eta", 100, -5 , 5);
+  TH1F* _egBxMin1Matched_eta = new TH2F("egBxMin1Matched_eta","egBxMin1Matched_eta", 100, -5 , 5);
+  TH1F* _egBxPlus1Matched_eta = new TH2F("egBxPlus1Matched_eta","egBxPlus1Matched_eta", 100, -5 , 5);
   //TH2F* _tauBxMatched_eta_phi = new TH2F("tauBxMatched_eta_phi","tauBxMatched_eta_phi", 100, -5 , 5, 72,-3.1416,3.1416);
   //TH2F* _jetBxMatched_eta_phi = new TH2F("jetBxMatched_eta_phi","jetBxMatched_eta_phi", 100, -5 , 5, 72,-3.1416,3.1416);
 
@@ -732,9 +732,9 @@ void ZeroBias_Timing::analyze(const edm::Event& iEvent, const edm::EventSetup& e
                       if (deltaR(ele, l1tEG)<0.5)
                         {
                           matchFound = true;
-                          if(ibx==-1)_egBxMin1Matched_eta_phi->Fill(l1tEG.eta() ,l1tEG.phi() );
-                          if(ibx==0)_egBx0Matched_eta_phi->Fill(l1tEG.eta() ,l1tEG.phi() );
-                          if(ibx==1)_egBxPlus1Matched_eta_phi->Fill(l1tEG.eta() ,l1tEG.phi() );
+                          if(ibx==-1)_egBxMin1Matched_eta_phi->Fill(l1tEG.eta());
+                          if(ibx==0)_egBx0Matched_eta_phi->Fill(l1tEG.eta());
+                          if(ibx==1)_egBxPlus1Matched_eta_phi->Fill(l1tEG.eta() );
                           
                           _egBxMatched->Fill(ibx);
                           //std::cout << std::endl << "IN: The BX of eg is " << ibx;
